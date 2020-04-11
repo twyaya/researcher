909e1bc64114c4cc71fd14ec3261ebafa478d71f
張皓偉 <otis8496@gmail.com>
2020-04-11 09:07:30 +0800

    增加頁面效果

--- js/init.js
+++ js/init.js
@@ -4,5 +4,25 @@ $(document).ready(function(){
 	AOS.init();
 	$('.button-collapse').sideNav();
 	$('.parallax').parallax();
+	
 });
-       
+
+	var options = {
+	  strings: ['我們是兩個高中生組成的團隊。'],
+	  typeSpeed: 70
+	};
+	
+	var typed = new Typed('#type-text', options);
+	typed.stop();
+	
+	var wow2 = new WOW(
+	  {
+		boxClass:     'wow2',
+	    callback:     function(box) {
+			typed.start();
+	    },
+	    scrollContainer: null 
+	  }
+	);
+	
+	wow2.init();
